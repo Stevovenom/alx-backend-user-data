7daea7367df4a104f5113142d76b5e6e68a082f8
stevovenom <stephenokoth2002@gmail.com>
2024-11-07 17:14:22 +0300

    db connection

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,6 +6,8 @@ Main file
 import re
 from typing import List
 import logging
+import os
+import mysql.connector # type: ignore
 
 
 class RedactingFormatter(logging.Formatter):
@@ -55,3 +57,39 @@ def get_logger() -> logging.Logger:
 
     logger.addHandler(target_handler)
     return logger
+
+def get_db() -> mysql.connector.connection.MYSQLConnection:
+    """
+    The connection to the ALX mysql environment
+    """
+    db_connect = mysql.connector.connect(
+        user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
+        password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
+        host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
+        database=os.getenv('PERSONAL_DATA_DB_NAME')
+    )
+    return db_connect
+
+def main() -> None:
+    """
+    obtain the database connection using get_db and retrive all role in the
+    users table and display each row under a filtered format.
+    """
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+
+    headers = [field[0] for field in cursor.description]
+    logger = get_logger()
+
+    for row in cursor:
+        info_answer = ''
+        for f, p in zip(row, headers):
+            info_answer += f'{p}={(f)}; '
+        logger.info(info_answer)
+
+    cursor.close()
+    db.close()
+
+    if __name__ == '__main__':
+        main()
