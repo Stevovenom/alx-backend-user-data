09a90ea5493a38ef9f5cee8f99b9011729064b56
stevovenom <stephenokoth2002@gmail.com>
2024-11-07 14:36:43 +0300

    update fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,14 +5,14 @@ def filter_datum(fields, redaction, message, separator):
     """
     function that returns the log message obfuscated
 
-    Args:
-        field: a list of strings representing all fields to obfuscate
-        redaction: a string representing by what the field will be obfuscated
-        message: a string representing the log message
-        separator: string representing characters seperating fields in message.
+    Arguments:
+        field (list): a list of strings representing all fields to obfuscate
+        redaction (str): a string representing by what the field will be obfuscated
+        message (str): a string representing the log message
+        separator (str): string representing characters seperating fields in message.
 
-    Return:
-        log message obfuscated
+    Returns:
+        str: log message obfuscated
     """
     return re.sub(r'(' + '|'.join(fields) + r')=[^' + separator + r']*',
                   r'\1=' + redaction, message)
