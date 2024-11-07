837179257be32d14dcf72bd778d5bae69393296e
stevovenom <stephenokoth2002@gmail.com>
2024-11-07 16:03:51 +0300

    fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -20,13 +20,15 @@ class RedactingFormatter(logging.Formatter):
     def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
-    
+
     def format(self, record: logging.LogRecord) -> str:
         """ Returns filtered values from log records """
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
 
+
 PII_FIELDS = ("name", "email", "password", "ssn", "phone")
+
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
