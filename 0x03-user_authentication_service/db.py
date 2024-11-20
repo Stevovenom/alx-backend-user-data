f9939dc985e9e4225ad9ad8d5ddf670dd116d420
stevovenom <stephenokoth2002@gmail.com>
2024-11-20 19:33:20 +0300

    task 2 fix

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -47,7 +47,7 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
-    
+
     def find_user_by(self, **kwargs) -> User:
         """
         Finds a user by arbitrary keyword arguments
